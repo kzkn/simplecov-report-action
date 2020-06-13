1779aa6fe06df29447beb7b201c046811c403d28
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2020-06-13 23:25:13 +0900

    debug

--- src/report.ts
+++ src/report.ts
@@ -7,7 +7,7 @@ import {Coverage} from './coverage'
 export async function report(coveredPercent: number, failedThreshold: number, coverage: Coverage): Promise<void> {
   const summaryTable = markdownTable([
     ['Covered', 'Threshold'],
-    [`${coveredPercent}%`, `${failedThreshold}%`]
+    [`${coveredPercent}%`, `${failedThreshold}%!!`]
   ])
   const coverageTable = markdownTable([
     ['Filename', 'Lines', 'Branches'],
